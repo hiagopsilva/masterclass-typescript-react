b1b43756b02288318fd9775de4a9b37435f8f9f1
hiagopsilva <hiagohps360@gmail.com>
2020-05-26 01:23:16 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import User from './components/User';
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      { users.map(user => <User key={user.email} user={user} />) }
     </div>
   );
 }
